86fef358566f4a5964422a688047529d1bbd8f41
unknown <bima.rafi60@sma.belajar.id>
2024-12-02 09:44:05 +0700

    six commit

--- hitungTotalHarga09.java
+++ hitungTotalHarga09.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class hitungTotalHarga09 {
     public static int hitungTotalHarga(int pilihanMenu, int banyakItem, String kodePromo) {
         int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
@@ -13,24 +12,26 @@ public class hitungTotalHarga09 {
         } else if (!kodePromo.equals("NONE")) {
             System.out.println("Kode promo " + kodePromo + " tidak valid.");
         }
-
         return hargaTotal;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Masukkan jumlah jenis menu yang ingin anda pesan: ");
+        int jumlahJenisMenu = sc.nextInt();
 
-        System.out.println("Masukkan nomor menu yang ingin anda pesan: ");
-        int pilihanMenu = sc.nextInt();
-        System.out.println("Masukkan jumlah item yang ingin dipesan: ");
-        int banyakItem = sc.nextInt();
-        sc.nextLine();
-
-        System.out.println("Masukkan kode promo (jika tidak ada, masukkan NONE): ");
-        String kodePromo = sc.nextLine();
+        for (int i = 0; i < jumlahJenisMenu; i++) {
+            System.out.println("Masukkan nomor menu untuk pesanan ke-" + (i + 1) + ": ");
+            int pilihanMenu = sc.nextInt();
+            System.out.println("Masukkan jumlah item untuk pesanan ke-" + (i + 1) + ": ");
+            int banyakItem = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Masukkan kode promo (jika tidak ada, masukkan NONE): ");
+            String kodePromo = sc.nextLine();
 
         int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem, kodePromo);
 
         System.out.println("Total harga untuk pesanan anda: Rp" + totalHarga);
+        }
     }
 }
